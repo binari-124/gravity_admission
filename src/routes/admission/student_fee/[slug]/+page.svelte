365325d4e6e6af7745945d7949d0f530094460af
binari <binari124@gmail.com>
2023-05-12 17:09:14 +0530

    @1

--- src/routes/admission/student_fee/[slug]/+page.svelte
+++ src/routes/admission/student_fee/[slug]/+page.svelte
@@ -357,7 +357,7 @@
                 </div>
 
                 <div class="mx-3 flex flex-row space-x-3 mt-2">
-                  <div class="mx-3 flex flex-col">
+                  <div class="mx-3 flex flex-col mt-3">
                     <label for="dd_cheque_number">Receipt Number</label>
                   </div>
 
