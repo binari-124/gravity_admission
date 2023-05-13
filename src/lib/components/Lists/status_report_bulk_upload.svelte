cb4b1f7dc2c4eb20a157b0e2c947551b377d26ee
binari <binari124@gmail.com>
2023-05-13 12:00:04 +0530

    @1

---
+++ src/lib/components/Lists/status_report_bulk_upload.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let returnData = null;
+    </script>
+    
+    {#if returnData}
+    <table class="w3-table-all w3-hoverable">
+        <thead>
+          <tr class="w3-light-grey">
+            <th>Email of the student</th>
+            <th>Error</th>
+          
+          </tr>
+        </thead>
+        {#each returnData as r}
+        <tr class="w3-hover-shadow">
+            <td>{r.email}</td>
+            <td>{r.message}</td>
+            <!-- <td>{student.batch?student.batch.name:"-"}</td>
+            <td>{student.branch?student.branch.name:"-"}</td>
+            <td>{student.email}</td>
+            <td><a class="w3-text-blue"  href="/students/{student._id}">{student._id}</a></td> -->
+        </tr>
+        {/each}
+      </table>
+    {/if}
---
+++ src/routes/_utils/xcl_upload.svelte
@@ -0,0 +1,142 @@
+<script>
+	export var url=`/api/files/students_excel`;
+	export var dontReload=false;
+
+	export var returnData;
+	let fileinput,fileinputFiles;
+	let fileType='official';
+	let fileName='';
+	import FormData from 'form-data';
+	
+	
+	const handleSubmit = async () => {
+	console.log("handling submit");
+	var formData = new FormData();
+	if(!fileinputFiles || !fileinputFiles[0])
+	{
+		alert("no file selected");
+		return;
+	}
+	let file = fileinputFiles[0];
+	
+	let token = localStorage.getItem("token");
+	//fetch upload of the image as the logo of the company!
+	// if(fileType=="" || fileName=="")
+	// {
+	// 	alert("please enter all the relevant fields(name+type)");
+	// 	return;
+	// }
+	formData.append("file", file);
+		const options = {
+		method: 'POST',
+		body: formData,
+		mode:'cors',
+		headers:{'Authorization':'Bearer '+token}
+		// If you add this, upload won't work
+		// headers: {
+		//   'Content-Type': 'multipart/form-data',
+		// }
+		};
+    
+    var res = await fetch(url, options);
+	if(res.status==200)
+	{
+		let response = await res.text();
+		try{
+			let data = await JSON.parse(response);
+			if(data.status == "success")
+			{
+				returnData = data.failed_transactions;
+				console.log(returnData);
+			}
+			else{
+				alert("Some error has occured, see console for more information.");
+				console.log(data);
+			}
+		}
+		catch(e){
+			alert("Some error has occured, see console for more information.")
+			console.log(e);
+		}
+	}
+	else{
+		let text = await res.text();
+		try{
+			
+			text = JSON.parse(text);
+			alert(text.message);
+		}
+		catch(e){
+			alert("some error occured");
+			console.log(e);
+		}
+
+	}
+
+
+            
+}
+	
+</script>
+<p>File size: 20MB Max</p>
+<div id="app" class=" w3-margin w3-padding w3-row">
+	<div class="w3-half">
+		<i class="fa fa-upload w3-center w3-green w3-padding w3-card w3-round" on:click={()=>{fileinput.click();}} aria-hidden="true"></i>
+		{#if fileinputFiles && fileinputFiles[0]}
+			<p>{fileinputFiles[0].name}</p>
+		{/if}
+		<br>
+        <!-- <div class="chan" on:click={()=>{fileinput.click();}} >Choose a File</div> -->
+		<!-- <input type="text" class="w3-input" placeholder="Name of the file" bind:value={fileName} /> -->
+		<br>
+	</div>
+
+	<div class="w3-half">
+		<!-- <label>File upload type</label> -->
+		<!-- <br> -->
+		<!-- <select class="w3-padding w3-margin w3-select w3-input" bind:value={fileType} >
+			<option value="academic">Academic</option>
+			<option value="materials_zip">Study Material Zip</option>
+			<option value="official">Official document</option>
+		</select> -->
+        <input style="{dontReload?'':'display:none'}" type="file" accept=".xls" bind:this={fileinput} bind:files={fileinputFiles} >
+		<br>
+		
+		<button class="w3-button w3-blue w3-margin w3-round" on:click={handleSubmit}>Submit</button>
+
+	</div>
+	
+		
+		
+	</div>
+
+
+
+<style>
+	.chan{
+		display:inline;
+		cursor:pointer;
+	}
+
+	i{
+		font-size: 300%;
+		cursor:pointer;
+	}
+	#app{
+	
+}
+ 
+	.upload{
+		display:flex;
+	height:50px;
+		width:50px;
+		cursor:pointer;
+	}
+	.avatar{
+		display:flex;
+		height:200px;
+		width:200px;
+	}
+</style>
+
+ 
