ed8468cdc591e395857318f6f1a103600e380914
binari <binari124@gmail.com>
2023-05-15 03:14:39 +0530

    @1

--- src/routes/admission/student_fee/[slug]/+page.svelte
+++ src/routes/admission/student_fee/[slug]/+page.svelte
@@ -539,7 +539,7 @@
                     >Remove Installment</button
                   >
 
-                  {#if !condition}
+                  {#if condition}
                     <button
                       class="btn float-right mr-2"
                       on:click|preventDefault={() => saveInstallment(index + 1)}
