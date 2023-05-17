7fab74b25341b962b407330cf4bd3677f888b8b1
binari <binari124@gmail.com>
2023-05-17 16:12:02 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -70,7 +70,7 @@
 					<input
 						type="text"
 						id="email"
-						
+						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
 						name="email"
 						bind:value={email}
 					/>
@@ -84,7 +84,7 @@
 					<input
 						type="password"
 						id="password"
-						
+						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
 						name="password"
 						bind:value={password}
 					/>
@@ -154,9 +154,9 @@
 		}
 	}
 
-	input {
+	/* input {
 		width: 50%;
-	}
+	} */
 
 	.main {
 		background: white;
