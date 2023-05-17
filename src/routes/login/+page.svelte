8691323e3ac19243af5556f0e6d7fe84a1f1fd1c
binari <binari124@gmail.com>
2023-05-17 16:45:05 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -81,13 +81,13 @@
 						class="block text-gray-700 text-sm font-bold mb-2 ml-3"
 						for="password">Password</label
 					>
-					<input
+					<!-- <input
 						type="password"
 						id="password"
 						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
 						name="password"
 						bind:value={password}
-					/>
+					/> -->
 				</div>
 
 				<!-- <input
