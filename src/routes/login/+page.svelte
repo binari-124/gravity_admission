50000c1e78d11a18f062f0ff3e326dbe406a476e
binari <binari124@gmail.com>
2023-05-17 16:50:41 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -62,7 +62,7 @@
 
 		<section class="mt-10">
 			<form class="flex flex-col" on:submit|preventDefault={handleSubmit}>
-				<div class="mb-6 pt-3 rounded bg-slate-600">
+				<div class="mb-6 pt-3 rounded bg-gray-200">
 					<label
 						class="block text-gray-700 text-sm font-bold mb-2 ml-3"
 						for="email">Email</label
@@ -70,7 +70,7 @@
 					<input
 						type="text"
 						id="email"
-						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4  focus:border-purple-600 transition duration-500 px-3 pb-3"
 						name="email"
 						bind:value={email}
 					/>
@@ -84,7 +84,7 @@
 					<input
 						type="password"
 						id="password"
-						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4  focus:border-purple-600 transition duration-500 px-3 pb-3"
 						name="password"
 						bind:value={password}
 					/>
