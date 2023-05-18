70b64db74664c486f1791950d603d8538bffc233
binari <binari124@gmail.com>
2023-05-18 12:36:32 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -56,7 +56,7 @@
 	<section class="flex flex-col md:flex-row h-screen items-center">
   
 	  <div class="bg-blue-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-		<img src="/home/binari/Documents/gravity_admission/static/images/glogoG.png" alt="" class="w-full h-full object-cover">
+		<img src="" alt="abcd" class="w-full h-full object-cover">
 	  </div>
 	
 	  <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
