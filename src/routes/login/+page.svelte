56db9c0c8ed3ef964b201b77ce2f8154aeb4cc7d
binari <binari124@gmail.com>
2023-05-18 16:40:29 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -66,7 +66,7 @@
 
 		<div
 			class="bg-gray-50 w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
-			flex items-center justify-center border-4 border-gray-300"
+			flex items-center justify-center border-l-2 border-gray-300"
 		>
 			<div class="w-full h-100">
 				<h1 class="text-xl font-bold">Welcome to Gravity</h1>
