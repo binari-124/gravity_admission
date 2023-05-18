88a2ba64bbd177c769ff2b940cfc6b39f8e6f55d
binari <binari124@gmail.com>
2023-05-18 12:26:34 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -51,22 +51,81 @@
 <svelte:head>
 	<title>Gravity Admin Panel</title>
 </svelte:head>
-<div class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
+
+<body>
+	<section class="flex flex-col md:flex-row h-screen items-center">
+  
+	  <div class="bg-blue-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+		<img src="bg.jpg" alt="" class="w-full h-full object-cover">
+	  </div>
+	
+	  <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+			flex items-center justify-center">
+	
+		<div class="w-full h-100">
+	
+		  <h1 class="text-xl font-bold">Tailwind Login</h1>
+		  <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
+	
+		  <form class="flex flex-col" on:submit|preventDefault={handleSubmit}>
+			<div>
+			  <label class="block text-gray-700">Email Address</label>
+			  <input type="email" name="" id="" placeholder="Enter Email Address" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete bind:value={email} required>
+			</div>
+	
+			<div class="mt-4">
+			  <label class="block text-gray-700">Password</label>
+			  <input type="password" name="" id="" placeholder="Enter Password" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+					focus:bg-white focus:outline-none" bind:value={password} required>
+			</div>
+	
+			<div class="text-right mt-2">
+			  <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
+			</div>
+	
+			<button type="submit" class="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+				  px-4 py-3 mt-6">Log In</button>
+		  </form>
+	
+		  <hr class="my-6 border-gray-300 w-full">
+	
+		  <button type="button" class="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+			<div class="flex items-center justify-center">
+			  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
+				<span class="ml-4">
+				Log in
+				with
+				Google
+				</span>
+			</div>
+		  </button>
+	
+		  <p class="mt-8">
+			Need an account? 
+			<a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">
+			  Create an account
+			</a>
+		  </p>
+	
+		  <p class="text-sm text-gray-500 mt-12">&copy; 2021 Talwind - All Rights Reserved.</p>
+		</div>
+  
+	  </div>
+	
+	</section>
+	
+  
+  </body>
+
+<!-- <div class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
 	<main
 		class="bg-gray-50 max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl"
 	>
-		<section>
-			<h3 class="font-bold text-2xl">Welcome to Gravity</h3>
-			<p class="text-gray-600 pt-2">Sign in to your account.</p>
-		</section>
-
+		
 		<section class="mt-10">
 			<form class="flex flex-col" on:submit|preventDefault={handleSubmit}>
 				<div class="mb-6 pt-3 rounded bg-gray-200">
-					<label
-						class="block text-gray-700 text-sm font-bold mb-2 ml-3"
-						for="email">Email</label
-					>
+					
 					<input
 						type="text"
 						id="email"
@@ -77,10 +136,7 @@
 				</div>
 
 				<div class="mb-6 pt-3 rounded bg-gray-200">
-					<label
-						class="block text-gray-700 text-sm font-bold mb-2 ml-3"
-						for="password">Password</label
-					>
+					
 					<input
 						type="password"
 						id="password"
@@ -90,11 +146,7 @@
 					/>
 				</div>
 
-				<!-- <input
-					class="w3-button w3-round w3-border w3-card"
-					type="submit"
-					value="Login"
-				/> -->
+				
 
 				<div class="flex justify-end">
 					<a
@@ -110,26 +162,12 @@
 			</form>
 		</section>
 	</main>
-	<div class="max-w-lg mx-auto text-center mt-12 mb-6">
-		<p class="text-white">
-			Don't have an account? <a
-				href="#"
-				class="font-bold hover:underline">Sign up</a
-			>.
-		</p>
-	</div>
+	
 
-	<footer class="max-w-lg mx-auto flex justify-center text-white">
-		<a href="#" class="hover:underline">Contact</a>
-		<span class="mx-3">•</span>
-		<a href="#" class="hover:underline">Privacy</a>
-	</footer>
-</div>
+	
+</div> -->
 
-<!-- <a href="/register">Register new user</a> -->
 
-<!-- <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.{username} {password}</strong></p> -->
-<!-- <p>saasdad</p> -->
 
 <p>created by: <a href="https://neoned71.com">neoned71</a></p>
 
@@ -178,8 +216,8 @@
 	}
 
 	.body-bg {
-		/* background-color: #9921e8;
-		background-image: linear-gradient(315deg, #9921e8 0%, #5f72be 74%); */
-		background-image: url('../../lib/images/pexels-karolina-grabowska-6256066\ \(1\).jpg');
+		background-color: #9921e8;
+		background-image: linear-gradient(315deg, #9921e8 0%, #5f72be 74%);
+		/* background-image: url('../../lib/images/pexels-karolina-grabowska-6256066\ \(1\).jpg'); */
 	}
 </style>
