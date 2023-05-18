4fcacc11dc3c65866c7f38d8761e3e827856ce20
binari <binari124@gmail.com>
2023-05-18 13:19:51 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -55,7 +55,7 @@
 <body>
 	<section class="flex flex-col md:flex-row h-screen items-center">
   
-	  <div class="bg-gray-50 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen p-20 mt-40">
+	  <div class="bg-gray-50 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen p-32 mt-60">
 		<img src="/images/glogoG.png" alt="abcd" class="w-fit h-fit object-cover">
 	  </div>
 	
