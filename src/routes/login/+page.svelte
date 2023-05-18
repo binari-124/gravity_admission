e9736d68b5cb747df702647658e63b28ebef9839
binari <binari124@gmail.com>
2023-05-18 12:35:18 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -56,7 +56,7 @@
 	<section class="flex flex-col md:flex-row h-screen items-center">
   
 	  <div class="bg-blue-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-		<img src="bg.jpg" alt="" class="w-full h-full object-cover">
+		<img src="/home/binari/Documents/gravity_admission/static/images/glogoG.png" alt="" class="w-full h-full object-cover">
 	  </div>
 	
 	  <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
@@ -64,7 +64,7 @@
 	
 		<div class="w-full h-100">
 	
-		  <h1 class="text-xl font-bold">Tailwind Login</h1>
+		  <h1 class="text-xl font-bold">Welcome to Gravity</h1>
 		  <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
 	
 		  <form class="flex flex-col" on:submit|preventDefault={handleSubmit}>
@@ -107,7 +107,7 @@
 			</a>
 		  </p>
 	
-		  <p class="text-sm text-gray-500 mt-12">&copy; 2021 Talwind - All Rights Reserved.</p>
+		  <p class="text-sm text-gray-500 mt-12">&copy; Gravity - All Rights Reserved.</p>
 		</div>
   
 	  </div>
