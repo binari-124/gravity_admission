632804b92924335a6b586dfdcf49d05eeaa72351
binari <binari124@gmail.com>
2023-05-18 11:37:19 +0530

    @1

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -67,8 +67,12 @@
 						class="block text-gray-700 text-sm font-bold mb-2 ml-3"
 						for="email">Email</label
 					>
-					<input type="text"
-					
+					<input
+						type="text"
+						id="email"
+						class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+						name="email"
+						bind:value={email}
 					/>
 				</div>
 
@@ -106,6 +110,20 @@
 			</form>
 		</section>
 	</main>
+	<div class="max-w-lg mx-auto text-center mt-12 mb-6">
+		<p class="text-white">
+			Don't have an account? <a
+				href="#"
+				class="font-bold hover:underline">Sign up</a
+			>.
+		</p>
+	</div>
+
+	<footer class="max-w-lg mx-auto flex justify-center text-white">
+		<a href="#" class="hover:underline">Contact</a>
+		<span class="mx-3">•</span>
+		<a href="#" class="hover:underline">Privacy</a>
+	</footer>
 </div>
 
 <!-- <a href="/register">Register new user</a> -->
