88871e634a270d4283e3a8e9aae5a49bec5534f2
binari <binari124@gmail.com>
2023-06-26 15:34:16 +0530

    @1

--- src/routes/_utils/_students/student_picker.svelte
+++ src/routes/_utils/_students/student_picker.svelte
@@ -3,8 +3,8 @@
     import { Token } from "$lib/dynamic_store.js";
     import { ApiUrl } from "$lib/static_store.js";
     import { get } from "svelte/store";
-    import StudentsList from "../../../lib/components/Lists/students.svelte";
-    // import StudentsList from "$lib/components/Lists/students.svelte";
+    // import StudentsList from "../../../lib/components/Lists/students.svelte";
+    import StudentsList from "$lib/components/Lists/students.svelte";
 
 
     var students = null;
