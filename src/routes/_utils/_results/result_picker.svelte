a8fefa20e53f8a3cdb9b9b558c2f9e6faaef1993
binari <binari124@gmail.com>
2023-10-17 17:43:17 +0530

    @1

--- src/routes/_utils/_results/result_picker.svelte
+++ src/routes/_utils/_results/result_picker.svelte
@@ -3,15 +3,16 @@
     import { Token } from "$lib/dynamic_store.js";
     import { ApiUrl } from "$lib/static_store.js";
     import { get } from "svelte/store";
-    // import StudentsList from "../../../lib/components/lists/students.svelte";
     import ResultsList from "$lib/components/lists/results.svelte";
 
+    var results = null;
 
-    var students = null;
-
-    var streams = [],
+    var 
+    // streams = [],
         branches = [],
         batches = [];
+        // tests = null;
+
 
     export let picker = true;
 
@@ -44,69 +45,92 @@
 
         body.batch = "-";
         body.branch = "-";
-        body.stream = "-";
+        // body.stream = "-";
         body.skip = 0;
         body.limit = 50;
 
-        await getStreams();
+        // await getStreams();
         await getBatches();
         await getBranches();
 
-        await getStudents();
+        await getResults();
+        // await getTest();
     });
 
-    const forceUpdate = async (_) => {};
-    ``;
+    // const forceUpdate = async (_) => {};
+    // ``;
 
-    async function getStudents(fresh = false) {
+    async function getResults(fresh = false) {
         var res;
         var token = localStorage.getItem("token");
 
-        console.log("fresh: " + fresh);
-        if (fresh) {
-            body.skip = 0;
-        }
-        // "/api/panel"
-        res = await fetch(loginPath + "/panel/students?" + serialize(body), {
+        // console.log("fresh: " + fresh);
+        // if (fresh) {
+        //     body.skip = 0;
+        // }
+        
+        // res = await fetch(loginPath + "/panel/students?" + serialize(body), {
+        //     mode: "cors",
+        //     method: "get",
+        //     headers: { Authorization: "Bearer " + token },
+        // });
+
+        // if (res.status == 200) {
+        //     try {
+        //         let response = await res.text();
+        //         console.log(response);
+        //         response = await JSON.parse(response);
+        //         if (response.status == "success") {
+        //             console.log("successfully grabbed students");
+        //             students = response.data;
+        //             students = students;
+        //             console.log(students);
+        //             // students=questions;
+        //         }
+        //     } catch (e) {
+        //         console.log("caught");
+
+        //         console.log(e);
+        //     }
+        // } else {
+        //     console.log(await res.text());
+        // }
+         res = await fetch(loginPath + "/panel/results/results_filter", {
             mode: "cors",
             method: "get",
             headers: { Authorization: "Bearer " + token },
         });
-
         if (res.status == 200) {
             try {
                 let response = await res.text();
                 console.log(response);
                 response = await JSON.parse(response);
-                if (response.status == "success") {
-                    console.log("successfully grabbed students");
-                    students = response.data;
-                    students = students;
-                    console.log(students);
-                    // students=questions;
-                }
+                results = response.data;
             } catch (e) {
                 console.log("caught");
 
                 console.log(e);
             }
+
+            // let data = JSON.parse(text);
         } else {
             console.log(await res.text());
+            user.email = "no logged";
         }
     }
 
-    function serialize(obj) {
-        var str = [];
-        for (var p in obj)
-            if (obj.hasOwnProperty(p)) {
-                if (obj[p] != "-") {
-                    str.push(
-                        encodeURIComponent(p) + "=" + encodeURIComponent(obj[p])
-                    );
-                }
-            }
-        return str.join("&");
-    }
+    // function serialize(obj) {
+    //     var str = [];
+    //     for (var p in obj)
+    //         if (obj.hasOwnProperty(p)) {
+    //             if (obj[p] != "-") {
+    //                 str.push(
+    //                     encodeURIComponent(p) + "=" + encodeURIComponent(obj[p])
+    //                 );
+    //             }
+    //         }
+    //     return str.join("&");
+    // }
 
     async function getStreams() {
         // console.log(body);
@@ -142,6 +166,31 @@
             console.log(await res.text());
             // user.email = "no logged";
         }
+    }
+
+    async function getTest() {
+        // console.log(body);
+        var token = localStorage.getItem("token");
+        var loginPath = get(ApiUrl);
+        let res;
+         res = await fetch(loginPath+'/panel/tests',{mode:'cors',method:'get',headers:{'Authorization':'Bearer '+token}});
+		if(res.status==200){
+			try{
+					let response= await res.text();
+					console.log(response);
+					response= await JSON.parse(response);
+					tests = response.data;	
+			}
+			catch(e){
+				console.log("caught");
+				console.log(e);
+			}
+		}
+		else{
+				console.log(await res.text());
+				user.email="no logged";
+					
+			}
     }
 
     async function getBatches() {
@@ -215,6 +264,8 @@
             // user.email = "no logged";
         }
     }
+
+    
 </script>
 
 <svelte:head>
@@ -226,59 +277,113 @@
         Picker
     {/if}
     <h4>Filters</h4>
-    <form on:submit|preventDefault={getStudents}>
-        {#if streams}
-            <p>Select Stream</p>
-            <br />
-            <select
-                class="w3-input w3-border w3-round w3-margin"
-                bind:value={body.stream}
+    <form on:submit|preventDefault={getResults}>
+        <div class="flex flex-row space-x-6">
+            <div class="w-1/4">
+                <label for="question_difficulty">Result Type</label>
+                <select
+                    id="question_difficulty"
+                    class="w3-input w3-border w3-round "
+                    bind:value={body.result_type}
+                >
+                    <option value="-">Select Result Type</option>
+                    <option value="jee_main">JEE Main</option>
+                    <option value="neet">NEET</option>
+                    <option value="general">General</option>
+                    
+                </select>
+            </div>
+            <!-- <div class="w-1/4">
+                <label for="question_difficulty"> Difficulty</label>
+                <select
+                    id="question_difficulty"
+                    class="w3-input w3-border w3-round mt-1"
+                    bind:value={body.difficulty}
+                >
+                    <option value="-">Select Difficulty</option>
+                    <option value="very-easy">Very Easy</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                    <option value="vary-hard">Very-Hard</option>
+                </select>
+            </div> -->
+            <!-- <div
+                class=" border-[1px] p-2 rounded-sm mx-1 my-2 w-1/4 h-10 mt-7 bg-gray-200 border-gray-300"
             >
-                <option value="-">Select Stream</option>
-                {#each streams as stream}
-                    <!-- <label>{}</label> -->
-                    <option value={stream._id}
-                        >{stream.name.toUpperCase()}</option
-                    >
-                    <!-- <input class="w3-checkbox w3-input" type="checkbox"  bind:group={body.streams} /> -->
-                {/each}
-            </select>
-        {/if}
+                <span>For Advanced?</span>
+                <input
+                    type="radio"
+                    checked={body.for_advanced}
+                    on:click={toggleAdvanced}
+                />
+            </div> -->
+            
+            <div class="w-1/4">
+                {#if branches && branches.length > 0}
+                <label for="question_topic">Branch</label>
+                <select
+                    id="question_topic"
+                    class="w3-input w3-border w3-round"
+                    bind:value={body.branch}
+                >
+                    <option value="-">Select Branch</option>
 
-        {#if branches}
-            <p>Select Branch</p>
-            <br />
-            <select
-                class="w3-input w3-border w3-round w3-margin"
-                bind:value={body.branch}
-            >
-                <option value="-">Select Branch</option>
-                {#each branches as branch}
-                    <!-- <label>{}</label> -->
-                    <option value={branch._id}
-                        >{branch.name.toUpperCase()}</option
-                    >
-                    <!-- <input class="w3-checkbox w3-input" type="checkbox"  bind:group={body.streams} /> -->
-                {/each}
-            </select>
-        {/if}
+                    {#each branches as branch}
+                        <option value={branch._id}
+                            >{branch.name.toUpperCase()}</option
+                        >
+                    {/each}
+                </select>
+                {/if}
+            </div>
 
-        {#if batches}
-            <p>Select Batches</p>
-            <br />
-            <select
-                class="w3-input w3-border w3-round w3-margin"
-                bind:value={body.batch}
-            >
-                <option value="-">Select Batch</option>
-                {#each batches as batch}
-                    <!-- <label>{}</label> -->
-                    <option value={batch._id}>{batch.name.toUpperCase()}</option
-                    >
-                    <!-- <input class="w3-checkbox w3-input" type="checkbox"  bind:group={body.streams} /> -->
-                {/each}
-            </select>
-        {/if}
+            <div class="w-1/4">
+                {#if batches && batches.length > 0}
+                <label for="question_topic">Batch</label>
+                <select
+                    id="question_topic"
+                    class="w3-input w3-border w3-round"
+                    bind:value={body.batch}
+                >
+                    <option value="-">Select Batch</option>
+
+                    {#each batches as batch}
+                        <option value={batch._id}
+                            >{batch.name.toUpperCase()}</option
+                        >
+                    {/each}
+                </select>
+                {/if}
+            </div>
+
+            <div class="w-1/4 ">
+                <label for="exam_date">Exam Date</label><br>
+                <input class="mt-2" type="date" name="exam date" id="exam_date" bind:value={body.exam_date}>
+            </div>
+
+
+            
+        </div>
+
+        <div class="flex flex-row space-x-6 mb-12">
+            <div class="w-1/2 ">
+                    
+                <input
+                    class="mt-7 border-[1px] w-full p-2"
+                    placeholder="Search by Exam Name or Exam ID"
+                    bind:value={body.exam_id}
+                />
+            </div>
+            <div class="w-1/2 ">
+                    
+                <input
+                    class="mt-7 border-[1px] w-full p-2"
+                    placeholder="Search by Batch Name and Batch ID"
+                    bind:value={body.batch_id}
+                />
+            </div>
+        </div>
 
         <input
             class="w3-button w3-round w3-border"
@@ -287,8 +392,8 @@
         />
     </form>
 
-    {#if students}
-        <h3>Students({body.skip}-{body.skip + body.limit})</h3>
+    {#if results}
+        <h3>Results({body.skip}-{body.skip + body.limit})</h3>
         <div class="flex">
             <p
                 class="w3-button w3-border w3-round w3-margin"
@@ -301,116 +406,115 @@
             </p>
         </div>
 
-        <div class="w3-padding w3-margin">
-            <ResultsList {students} />
-        </div>
+        <!-- <div class="w3-padding w3-margin">
+            <ResultsList {results} />
+        </div> -->
     {:else}
-        <p>No students found!</p>
+        <p>No results found!</p>
     {/if}
 </div>
 
 <style>
-	*{
-		color:rgb(51, 51, 51);
-	}
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
+    * {
+        color: rgb(51, 51, 51);
+    }
+    .content :global(h2) {
+        font-size: 1.4em;
+        font-weight: 500;
+    }
 
-	.content :global(pre) {
-		background-color: #dfc502;
-		box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
+    .content :global(pre) {
+        background-color: #dfc502;
+        box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
+        padding: 0.5em;
+        border-radius: 2px;
+        overflow-x: auto;
+    }
 
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
+    .content :global(pre) :global(code) {
+        background-color: transparent;
+        padding: 0;
+    }
 
-	.content :global(ul) {
-		line-height: 1.5;
-	}
+    .content :global(ul) {
+        line-height: 1.5;
+    }
 
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	}
+    .content :global(li) {
+        margin: 0 0 0.5em 0;
+    }
 
-	.container{
-		
-		width:100%;
-		/* border:1px solid rgb(140, 140, 140); */
-		border-radius: 5px;
-		margin:10px;
-		padding: 10px;
-		display: flex;
-		flex-direction: column;
-		align-items: left;
+    .container {
+        width: 100%;
+        /* border:1px solid rgb(140, 140, 140); */
+        border-radius: 5px;
+        margin: 10px;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: left;
 
-		/* background: linear-gradient(45deg,rgb(134, 60, 253),var(--template-color)); */
+        /* background: linear-gradient(45deg,rgb(134, 60, 253),var(--template-color)); */
+    }
+    img {
+        display: block;
+        width: 150px;
+    }
+    .certificate {
+        font-size: 120px;
+        font-family: "MonteCarlo", cursive;
+    }
+    .organization {
+        color: black;
+        font-size: 80px;
+        font-family: "MonteCarlo", cursive;
+    }
+    .cont {
+        font-family: "MonteCarlo", cursive;
+        font-size: 30px;
+    }
+    .straight {
+        font-family: "Merriweather", serif;
 
-	}
-	img{
-		display:block;
-		width:150px;
-	}
-	.certificate{
-		font-size: 120px;
-		font-family: 'MonteCarlo', cursive;
-	}
-	.organization{
-		color:black;
-		font-size: 80px;
-		font-family: 'MonteCarlo', cursive;
-	}
-	.cont{
-		font-family: 'MonteCarlo', cursive;
-		font-size: 30px;
-	}
-	.straight{
-		font-family: 'Merriweather', serif;
+        /* font-family: 'MonteCarlo', cursive; */
+    }
+    .flex {
+        margin-top: 30px;
+        display: flex;
+        align-items: stretch;
+    }
+    .flexit {
+        flex: 1;
+        padding: 20px;
+    }
+    p {
+        /* width:100%; */
+        margin: 0;
+        text-align: left;
+    }
+    h1 {
+        margin: 00px;
+    }
+    div {
+        align-self: left;
+        justify-self: left;
+    }
+    .abs {
+        position: absolute;
+        top: 120px;
+        right: 130px;
+    }
+    a {
+        text-decoration: none;
+    }
 
-		/* font-family: 'MonteCarlo', cursive; */
-	}
-	.flex{
-		margin-top: 30px;
-		display:flex;
-		align-items: stretch;
-	}
-	.flexit{
-		flex:1;
-		padding:20px;
-	}
-	p{
-		/* width:100%; */
-		margin:0;
-		text-align: left;
-	}
-	h1{
-		margin:00px;
-	}
-	div{
-		align-self: left;
-		justify-self: left;
-	}
-	.abs{
-		position:absolute;
-		top:120px;
-		right:130px;
-	}
-	a{
-		text-decoration: none;;
-	}
+    h2,
+    h3 {
+        font-size: 250%;
+    }
 
-	h2,h3{
-		font-size:250%;
-	}
-
-	button{
-		display:inline;
-		width:200px;
-	}
+    button {
+        display: inline;
+        width: 200px;
+    }
 </style>
--- src/routes/branch/+page.svelte
+++ src/routes/branch/+page.svelte
@@ -1,67 +1,82 @@
-
-
 <script>
-	import BranchesList from '../../lib/components/lists/branches.svelte';
-	import {onMount} from 'svelte';
-	import {Token} from '../_utils/dynamic_store.js';
-	import {ApiUrl} from '../_utils/static_store.js';
-	import { get } from 'svelte/store';
-// import { roomId } from '../room/[slug].svelte';
+	import BranchesList from "../../lib/components/lists/branches.svelte";
+	import { onMount } from "svelte";
+	import { Token } from "../_utils/dynamic_store.js";
+	import { ApiUrl } from "../_utils/static_store.js";
+	import { get } from "svelte/store";
+	// import { roomId } from '../room/[slug].svelte';
 	// export var roomId;
 
 	var branches = null;
-	 var loginPath=get(ApiUrl);
-	
-	 onMount(async ()=>{
+	var loginPath = get(ApiUrl);
+
+	onMount(async () => {
 		const urlParams = new URLSearchParams(window.location.search);
-    	//  urlParams.has('class_number');
-		
-		let classQuery="";
-		
-		if(urlParams.has('class_number'))
-		{
-			let classNumber =urlParams.get("class_number");
-			classQuery="?class_number="+classNumber;
+		//  urlParams.has('class_number');
+
+		let classQuery = "";
+
+		if (urlParams.has("class_number")) {
+			let classNumber = urlParams.get("class_number");
+			classQuery = "?class_number=" + classNumber;
 		}
-		
+
 		// localStorage.setItem("token","some value");
-		
+
 		var token = localStorage.getItem("token");
-		if(!token)
-		{
+		if (!token) {
 			console.log("yes");
-			location.href="/login";
+			location.href = "/login";
 		}
 		Token.set(token);
 
-
 		// console.log(loginPath+'/auth/whoami');
-		const res = await fetch(loginPath+'/panel/branches'+classQuery,{mode:'cors',method:'get',headers:{'Authorization':'Bearer '+token}});
-		if(res.status==200){
-			try{
-					let response= await res.text();
-					// console.log(response);
-					response= await JSON.parse(response);
-					branches = response.data;
-					console.log(branches);
-					console.log("branches are loaded");
-			}
-			catch(e){
+		const res = await fetch(loginPath + "/panel/branches" + classQuery, {
+			mode: "cors",
+			method: "get",
+			headers: { Authorization: "Bearer " + token },
+		});
+		if (res.status == 200) {
+			try {
+				let response = await res.text();
+				// console.log(response);
+				response = await JSON.parse(response);
+				branches = response.data;
+				console.log(branches);
+				console.log("branches are loaded");
+			} catch (e) {
 				console.log("caught");
 				console.log(e);
 			}
+		} else {
+			console.log(await res.text());
+			user.email = "no logged";
 		}
-		else{
-					console.log(await res.text());
-					user.email="no logged";
-					
-				}
-		// 
-	})
+		//
+	});
 </script>
 
+<h3>Branches</h3>
+<a href="/branch/create/">
+	<button class="w3-button w3-border w3-round"
+		>Add new <i class="w3-text-grey fas fa-plus" /></button
+	>
+</a>
+
+<hr />
+<div class="w3-round">
+	{#if branches}
+		<!-- <p>sad</p> -->
+		<BranchesList {branches} />
+	{:else}
+		<p>No Branch Present</p>
+	{/if}
+</div>
+
 <style>
-	h1, figure, p {
+	h1,
+	figure,
+	p {
 		text-align: center;
 		margin: 0;
 	}
@@ -70,7 +85,7 @@
 		/* font-size: 2.8em; */
 		text-transform: uppercase;
 		font-weight: 700;
-		margin:5px;
+		margin: 5px;
 		/* font-family: 'MonteCarlo', cursive; */
 	}
 
@@ -84,71 +99,53 @@
 		margin: 0 0 1em 0;
 	}
 
-	
-
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
 		}
 	}
 
-	.item{
-		padding:10px;
-		display:block;
-		width:100%;
+	.item {
+		padding: 10px;
+		display: block;
+		width: 100%;
 		text-decoration: none;
-		margin:1px;
+		margin: 1px;
 		background-color: rgb(255, 255, 255);
 	}
-	.item:hover{
+	.item:hover {
 		/* background-color: rgb(191, 201, 255); */
-		cursor:pointer;
+		cursor: pointer;
 	}
-	p{
+	p {
 		text-align: left;
 		/* font-family: 'Merriweather', serif; */
 	}
 
 	.grid-container {
-	display: grid;
-	grid-template-columns: auto auto auto;
-	grid-gap: 10px;
-	background-color: rgb(255, 255, 255);
-	/* padding: 10px; */
+		display: grid;
+		grid-template-columns: auto auto auto;
+		grid-gap: 10px;
+		background-color: rgb(255, 255, 255);
+		/* padding: 10px; */
 	}
 
 	.grid-container > div {
-	/* background-color: rgba(255, 255, 255, 0.8); */
-	/* border: 1px solid black; */
-	text-align: center;
-	/* font-size: 30px; */
+		/* background-color: rgba(255, 255, 255, 0.8); */
+		/* border: 1px solid black; */
+		text-align: center;
+		/* font-size: 30px; */
 	}
-	a{
+	a {
 		text-decoration: none;
 	}
 
-	img{
-		width:100px;
+	img {
+		width: 100px;
 	}
 
-	h2,h3{
-		font-size:300%;
+	h2,
+	h3 {
+		font-size: 300%;
 	}
 </style>
-
-
-<h3>Branches</h3>
-<a href="/branch/create/">
-	<button class="w3-button w3-border w3-round">Add new <i class="w3-text-grey fas fa-plus"></i></button>
-</a>
-
-
-<hr>
-<div class="w3-round">
-	{#if branches}
-		<!-- <p>sad</p> -->
-		<BranchesList {branches} />
-	{:else}
-		<p>No Branch Present</p>
-	{/if}
-</div>
