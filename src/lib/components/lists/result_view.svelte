f77cae267d4fa3e199f278d88b15edafe1a843f4
binari <binari124@gmail.com>
2023-11-04 12:01:20 +0530

    @1

---
+++ src/lib/components/lists/result_view.svelte
@@ -0,0 +1,83 @@
+<script>
+    export let total_test_results = null;
+    console.log("wxyz");
+    console.log(total_test_results);
+    import { ApiUrl } from "../../_utils/static_store.js";
+    import { get } from "svelte/store";
+  
+  </script>
+  
+  <div class="w3-round">
+    <p>This is result_newefwet{total_test_results}</p>
+    {#if total_test_results}
+      <table class="w3-table-all w3-hoverable" id="tabledata" width="100%">
+        <thead>
+          <tr class="w3-light-grey">
+            <th>Name</th>
+            <th>Student ID</th>
+            <th>Student Number</th>
+            <th>Max Marks</th>
+            <th>Marks Obtained</th>
+  
+            <!-- <th>Delete?</th> -->
+          </tr>
+        </thead>
+        {#each total_test_results as result}
+          <tr class="w3-hover-shadow">
+            <td>{result["Student Name"]}</td>
+            <!-- <td>{result.student._id}</td> -->
+            <td>{result["Student ID"]}</td>
+            <td>{result["Phone No"]}</td>
+            <td>{result["Max Marks"]}</td>
+            <td>{result["Marks Obtained"]}</td>
+            <!-- <td>
+              {#each subject.streams as stream}
+                {stream.name},
+              {/each}
+            </td>
+            <td
+              ><a class="w3-text-blue" href="/subjects/{subject._id}"
+                >{subject._id}</a
+              ></td
+            >
+            <td
+              ><button
+                class="w3-text-red w3-center"
+                on:click={() => {
+                  handleDelete(subject._id);
+                }}>Delete</button
+              ></td
+            > -->
+          </tr>
+        {/each}
+        <!-- {#each result_new as result}
+          <tr class="w3-hover-shadow">
+            <td>{result.student.name}</td>
+            <td>{result.test}</td>
+            <td>{result.student.phone}</td>
+            <td>{result.max_marks}</td>
+            <td>{result.total}</td>
+            <td>
+              {#each subject.streams as stream}
+                {stream.name},
+              {/each}
+            </td>
+            <td
+              ><a class="w3-text-blue" href="/subjects/{subject._id}"
+                >{subject._id}</a
+              ></td
+            >
+            <td
+              ><button
+                class="w3-text-red w3-center"
+                on:click={() => {
+                  handleDelete(subject._id);
+                }}>Delete</button
+              ></td
+            >
+          </tr>
+        {/each} -->
+      </table>
+    {/if}
+  </div>
+  
--- src/routes/results/result/[slug]/+page.svelte
+++ src/routes/results/result/[slug]/+page.svelte
@@ -11,6 +11,9 @@
   // import ResultsList from "$lib/components/lists/results.svelte";
   import { bubble } from "svelte/internal";
   import { read, utils, writeFileXLSX } from "xlsx";
+  import ResultsView from "$lib/components/lists/result_view.svelte";
+    import ResultView from "../../../../lib/components/lists/result_view.svelte";
+
 
   var result_new = null;
   // let result_new = [];
@@ -228,80 +231,7 @@
     >Export to Excel</button
   >
 </div>
-<div class="w3-round">
-  <p>This is result_newefwet{result_new}</p>
-  {#if result_new}
-    <table class="w3-table-all w3-hoverable" id="tabledata" width="100%">
-      <thead>
-        <tr class="w3-light-grey">
-          <th>Name</th>
-          <th>Student ID</th>
-          <th>Student Number</th>
-          <th>Max Marks</th>
-          <th>Marks Obtained</th>
-
-          <!-- <th>Delete?</th> -->
-        </tr>
-      </thead>
-      <!-- {#each total_test_results as result}
-        <tr class="w3-hover-shadow">
-          <td>{result["Student Name"]}</td>
-          <td>{result.student._id}</td>
-          <td>{result["Student ID"]}</td>
-          <td>{result["Phone No"]}</td>
-          <td>{result["Max Marks"]}</td>
-          <td>{result["Marks Obtained"]}</td>
-          <td>
-            {#each subject.streams as stream}
-              {stream.name},
-            {/each}
-          </td>
-          <td
-            ><a class="w3-text-blue" href="/subjects/{subject._id}"
-              >{subject._id}</a
-            ></td
-          >
-          <td
-            ><button
-              class="w3-text-red w3-center"
-              on:click={() => {
-                handleDelete(subject._id);
-              }}>Delete</button
-            ></td
-          >
-        </tr>
-      {/each} -->
-      {#each result_new as result}
-        <tr class="w3-hover-shadow">
-          <td>{result.student.name}</td>
-          <!-- <td>{result.student._id}</td> -->
-          <td>{result.test}</td>
-          <td>{result.student.phone}</td>
-          <td>{result.max_marks}</td>
-          <td>{result.total}</td>
-          <!-- <td>
-            {#each subject.streams as stream}
-              {stream.name},
-            {/each}
-          </td>
-          <td
-            ><a class="w3-text-blue" href="/subjects/{subject._id}"
-              >{subject._id}</a
-            ></td
-          >
-          <td
-            ><button
-              class="w3-text-red w3-center"
-              on:click={() => {
-                handleDelete(subject._id);
-              }}>Delete</button
-            ></td
-          > -->
-        </tr>
-      {/each}
-    </table>
-  {/if}
-</div>
+<ResultView {total_test_results} />
 
 <style>
   * {
